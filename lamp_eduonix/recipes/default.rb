c1f390a5a95da59e366fb2182e7fe255d962fa12
Srinivasa Nagaraja <sri.lynx@gmail.com>
2019-03-09 04:09:46 +0000

    package resources added to default lamp cookbook

--- lamp_eduonix/recipes/default.rb
+++ lamp_eduonix/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+#
+
+%w(httpd mariadb-server unzip git vim).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
